3d65d737a40bba4ae26b03f36c68c72c6d0b3448
zmil44 <zachary.j.miller@outlook.com>
2018-08-14 12:27:39 -0400

    initial commit

---
+++ Assignment 6.2/Tape.cs
@@ -0,0 +1,83 @@
+﻿/*Zachary Miller
+ * 12/11/2017
+ * assignment 6.2
+ * The purpose of this program is to create the class and sub classes for tape. It also has a ToString modifier in each class and subclass.
+ * Tape.cs
+ * Bellevue University
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6._2
+{
+    class Tape
+    {
+
+        public Tape(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\nLength: {1}\nWdith: {2}",GetType(),Length,Width);
+        }
+    }
+
+    class AdhesiveTape : Tape
+    {
+        int StickyRating;
+        public AdhesiveTape(int length, int width, int stickiness)
+            :base(length, width)
+        {
+            StickyRating = stickiness;
+        }
+
+        public int Stickiness
+        {
+            get
+            {
+                return StickyRating;
+            }
+            set
+            {
+                if (value >= 1 && value <= 10)
+                    StickyRating = value;
+                else
+                    StickyRating = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\nStickiness Rating: {1}",base.ToString(),(StickyRating == 0)?"Invalid":Stickiness.ToString());
+        }
+    }
+
+    class VideoTape:Tape
+    {
+        public int Time { get; set; }
+        public VideoTape(int length, int width, int time)
+            :base(length,width)
+        {
+            if (time >= 1 && time <= 180)
+                Time = time;
+            else
+                Time = 0;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\nTime: {1}",base.ToString(),(Time==0)?"Invalid":Time.ToString());
+        }
+    }
+
+
+}
---
+++ Assignment 6.2/TapeDemo.cs
@@ -0,0 +1,31 @@
+﻿/*Zachary Miller
+ * 12/11/2017
+ * assignment 6.2
+ * The purpose of this program is to create instances of the tape class and the subclasses VideoTape and AdhesiveTape
+ * GirlScout.cs
+ * Bellevue University
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6._2
+{
+    class TapeDemo
+    {
+        static void Main(string[] args)
+        {
+            Tape tape = new Tape(50, 10);
+            VideoTape videoTape = new VideoTape(60, 6, 150);
+            AdhesiveTape adhesiveTape = new AdhesiveTape(20, 9, 5);
+
+            Console.WriteLine(tape);
+            Console.WriteLine(videoTape);
+            Console.WriteLine(adhesiveTape);
+
+            Console.ReadLine();
+        }
+    }
+}
